b6416f01cd4663c5de1adb0dd2b05dbdf475469d
Michael Koderisch <code@koderisch.co.uk>
2020-03-05 09:32:20 +0000

    postgreSQL implemented

--- server/lib/globals.js
+++ server/lib/globals.js
@@ -1,6 +1,7 @@
 const Globals = {
     mongoDbUrl: "mongodb://localhost:27017",
     mongoDbName: "pbTasks",
+    postgreDbUrl: "postgres://michaelk:michaelk@localhost:5432/michaelk"
   };
 
   module.exports = Globals;
--- server/lib/user-db.js
+++ server/lib/user-db.js
@@ -1,113 +1,41 @@
-const MongoClient = require("mongodb").MongoClient;
-
 const globals = require("../lib/globals"); //load global variables
 
+var pgp = require("pg-promise")(/* options */);
+
 class UserDb {
-  /**
-   * Create and return a new MongoClient
-   * @method connect
-   * @async
-   */
-  async connect() {
-    const client = MongoClient.connect(globals.mongoDbUrl, {
-      useNewUrlParser: true
-    });
-    console.log("Connected correctly to mongo server");
-    return client;
-  }
-
-  /**
-   * Find documents in Mongo collection
-   * @method find
-   * @async
-   * @param {string} collection - name of the collection
-   * @param {string} query - query string for find
-   * @param {object} sort - object containing field to sort by (e.g. { name: 1 } to sort by field "name")
-   * @param {object} projection - object containing fields to return with results (e.g. { name: true, description: true } to return fields "name" and "description" only)
-   */
-  async find(collection, query, sort = {}, projection = {}) {
-    const client = await this.connect();
-    const db = client.db(globals.mongoDbName);
-
-    try {
-      const r = await db
-        .collection(collection)
-        .find(query, {
-          sort: sort,
-          projection: projection
-        })
-        .toArray();
-      return r;
-    } catch (err) {
-      console.log(err.stack);
-    } finally {
-      client.close();
-    }
-  }
-
-  /**
-   * Find documents in Mongo collection
-   * @method findOne
-   * @async
-   *
-   * @param {string} collection - name of the collection
-   * @param {string} query - query string for find
-   * @param {object} projection - object containing fields to return with results (e.g. { name: true, description: true } to return fields "name" and "description" only)
-   */
-  async findOne(collection, query, projection) {
-    const client = await this.connect();
-    const db = client.db(globals.mongoDbName);
-
-    try {
-      // find a single document
-      const r = await db.collection(collection).findOne(query, {
-        projection: projection
-      });
-      //console.log("r", r);
-      return r;
-    } catch (err) {
-      console.log(err.stack);
-    } finally {
-      //close connection
-      client.close();
-    }
-  }
-
   getAll(callback) {
-    const collection = "users";
-    const query = {};
-    const sort = { name: 1 };
-    const projection = {};
-    this.find(collection, query, sort, projection)
-      .catch(err => {
-        console.error("dbase.find error", err);
-        callback(null, err);
+    const db = pgp(globals.postgreDbUrl);
+    db.any("select * from users", [])
+      .then(data => {
+        console.log("DATA:", data);
+        callback(data);
       })
-      .then(result => {
-        console.log(result);
-        callback(result);
-      });
+      .catch(error => {
+        console.error("dbase error", error);
+        callback(null, error);
+      })
+      .finally(db.$pool.end);
   }
 
   logIn(userName, password, callback) {
-    const collection = "users";
-    const query = { name: userName };
-    const sort = {};
-    const projection = {};
-    this.findOne(collection, query, sort, projection)
-      .catch(err => {
-        console.error("dbase.find error", err);
-        callback(null, err);
-      })
-      .then(result => {
-        console.log(result);
+    const db = pgp(globals.postgreDbUrl);
+    db.any("select * from users where name = $1", [userName])
+      .then(data => {
+        console.log("DATA:", data);
+        console.log("password:", password);
+        const result = data[0];
         if (result.password === password) {
           callback(result);
         } else {
           const err = "passwords don't match";
           callback(null, err);
         }
-      });
+      })
+      .catch(error => {
+        console.error("dbase error", error);
+        callback(null, error);
+      })
+      .finally(db.$pool.end);
   }
 }
 
